d0d019c06fb2be4a9a81188c1f89d3821e0d2218
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-11-14 11:14:55 -0300

    Shopapi refactoring

--- shop-api/src/main/java/com/ewecarreiras/shopapi/Controller/ShopController.java
+++ shop-api/src/main/java/com/ewecarreiras/shopapi/Controller/ShopController.java
@@ -1,23 +1,17 @@
 package com.ewecarreiras.shopapi.Controller;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ewecarreiras.shopapi.dto.ShopDto;
 import com.ewecarreiras.shopapi.entity.Shop;
 import com.ewecarreiras.shopapi.entity.ShopItem;
 import com.ewecarreiras.shopapi.event.KafkaClient;
 import com.ewecarreiras.shopapi.repository.ShopRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/shop")
@@ -30,16 +24,16 @@ public class ShopController {
     @GetMapping
     public List<ShopDto> getShop() {
         return shopRepository.findAll()
-                .stream()
-                .map(shop -> ShopDto.convert(shop))
-                .collect(Collectors.toList());
+            .stream()
+            .map(shop -> ShopDto.convert(shop))
+            .collect(Collectors.toList());
     }
 
     @PostMapping
     public ShopDto saveShop(@RequestBody ShopDto shopDto) {
         shopDto.setIdentifier(UUID.randomUUID().toString());
         shopDto.setDateShop(LocalDate.now());
-//         shopDto.setStatus("PENDING");
+        shopDto.setStatus("PENDING");
         Shop shop = Shop.convert(shopDto);
         for (ShopItem shopItem : shop.getItems()) {
             shopItem.setShop(shop);
